341f1e319900f09d4822e2554e0bb96de04a1595
frombarnaul <pajh5@yandex.ru>
2024-04-12 09:33:33 +0700

    Добавьте файлы проекта.

---
+++ Bits.cs
@@ -0,0 +1,33 @@
+﻿public struct Bits
+{
+    private readonly ulong _value;
+
+    public Bits(ulong value)
+    {
+        _value = value;
+    }
+
+    // Неявное приведение из long в Bits
+    public static implicit operator Bits(long value)
+    {
+        return new Bits((ulong)value);
+    }
+
+    // Неявное приведение из int в Bits
+    public static implicit operator Bits(int value)
+    {
+        return new Bits((ulong)value);
+    }
+
+    // Неявное приведение из byte в Bits
+    public static implicit operator Bits(byte value)
+    {
+        return new Bits((ulong)value);
+    }
+
+    // Перегрузка ToString для удобства вывода
+    public override string ToString()
+    {
+        return _value.ToString();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace cast
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long longValue = 123456789012345;
+            int intValue = 123456;
+            byte byteValue = 123;
+
+            Bits bitsFromLong = longValue; // Неявное приведение из long в Bits
+            Bits bitsFromInt = intValue;   // Неявное приведение из int в Bits
+            Bits bitsFromByte = byteValue; // Неявное приведение из byte в Bits
+
+            Console.WriteLine($"Bits from long: {bitsFromLong}");
+            Console.WriteLine($"Bits from int: {bitsFromInt}");
+            Console.WriteLine($"Bits from byte: {bitsFromByte}");
+        }
+    }
+}
